2e3b46816bd5bcfe86426310e26b245947c7f601
Finch Juma <finch.juma@student.moringaschool.com>
2026-02-08 20:22:13 +0300

    Add constructor function for Place objects

---
+++ script.js
@@ -0,0 +1,7 @@
+function Place(location, landmarks, timeOfYear, notes, image){
+    this.location = location;
+    this.landmarks = landmarks;
+    this.timeOfYear = timeOfYear;
+    this.notes = notes;
+    this.image = image;
+}
