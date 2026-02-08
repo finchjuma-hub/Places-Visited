c6f022a7e2c31e096b2181c71fc68a7a2916f2c5
Finch Juma <finch.juma@student.moringaschool.com>
2026-02-08 21:04:35 +0300

    Add places visited

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ Place.prototype.displayDetails = function(){
     return `${this.location} | Landmarks: ${this.landmarks.join(", ")} 
   | Best time: ${this.timeOfYear} | Notes: ${this.notes}`;
 };
+
+const places =[
+    new Place("Kisumu", ["Citam Kisumu", "Kisumu Town"], "Spring", "Perfect place to find fish."),
+    new Place("Nairobi", ["Wilson Airport", "University of Nairobi"], "Autum", "Great balance for study and exploration."),
+    new Place("Mombasa", ["Show room", "Beautiful hotels", "White sandy beaches", "Fort Jesus"], "Summer", "Coastal city with rich history"),
+    new Place("Kakamega", ["Mumias sugar factory", "Kakamega Forest"], "Rainy season", "Known for lush rainforest and biodiversity."),
+    new Place("Tigoni", ["Tea Plantations"], "Cool season", "Beautiful highlands with scenic tea farms."),
+    new Place("Naivasha", ["Artcaffe mall"], "Spring", "Popular for lakeside relaxation and hiking.")
+];
